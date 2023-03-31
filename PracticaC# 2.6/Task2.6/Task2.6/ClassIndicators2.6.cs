55008d93316b471df37adbac332783a436c9be6e
RedFyru <mr.mlguy95@gmail.com>
2023-03-31 12:34:15 +0700

    gggg

---
+++ PracticaC# 2.6/Task2.6/Task2.6/ClassIndicators2.6.cs
@@ -0,0 +1,10 @@
+namespace WetherApp
+{
+    class Indicators
+    {
+        public float Temp { get; set; }
+        public float feels_like { get; set; }
+        public string description { get; set; }
+        public float speed { get; set; }
+    }
+}
--- PracticaC# 2.6/Task2.6/Task2.6/ClassWeather2.6.cs
+++ PracticaC# 2.6/Task2.6/Task2.6/ClassWeather2.6.cs
@@ -8,12 +8,4 @@
         public Indicators[] weather { get; set; }
         public Indicators wind { get; set; }
     }
-
-    class Indicators
-    {
-        public float Temp { get; set; }
-        public float feels_like { get; set; }
-        public string description { get; set; }
-        public float speed { get; set; }
-    }
 }
