ee107bce97c2246be60a0c335fcff00e916a49b0
Смоленский Глеб Игоревич <gr612_smgig@ttit.local>
2023-04-01 14:47:16 +0700

    ggg

--- PracticaC# 2.1/Task 2.1/Task 2.1/Program.cs
+++ PracticaC# 2.1/Task 2.1/Task 2.1/Program.cs
@@ -19,115 +19,71 @@
 // }
 
 //using System;
-//using System.Collections;
 //using System.Collections.Generic;
-//using System.Linq;
-//Задание 2.1(2)(НЕДОДЕЛАНО)
-//namespace Task_2._1
+////Задание 2.1(2)
+
+//namespace RKIS
 //{
 //    class Program
 //    {
+//        static string sort(string number1)
+//        {
+//            char[] number = number1.ToCharArray();
+//            for (int i = 0; i < number.Length - 1; i++)
+//            {
+//                for (int j = 0; j < number.Length - i - 1; j++)
+//                {
+//                    if (int.Parse(number[j].ToString()) > int.Parse(number[j + 1].ToString()))
+//                    {
+//                        // меняем элементы местами
+//                        char temp = number[j];
+//                        number[j] = number[j + 1];
+//                        number[j + 1] = temp;
+//                    }
+//                }
+//            }
+//            return new string(number);
+//        }
+
 //        static void Main()
 //        {
-//            int[] candidates = new int[7] { 10, 1, 2, 7, 6, 1, 5 };
+//            int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
 //            int target = 8;
-//            int sum = 0;
-//            List<int> numbers = new List<int>();
-//            List<int> check = new List<int>();
-//            List<List<int>> res = new List<List<int>>();
+//            Console.WriteLine($"Цель- {target}");
+//            Console.WriteLine("Первоначальный массив: ");
 //            Console.WriteLine(String.Join(", ", candidates));
-//            for (int i = 0; i < candidates.Length; i++)
+//            List<string> result = new List<string>();
+//            Console.WriteLine("Список всех вариантов: ");
+//            for (int i = 0; i < Math.Pow(2, candidates.Length); i++)
 //            {
-//                if (candidates[i] <= target)
+//                int sum = 0;
+//                string res = "";
+//                double b = i;
+//                int r = 0;
+//                while (r < candidates.Length)
 //                {
-//                    numbers.Add(candidates[i]);
+//                    double c = b;
+//                    b = Math.Floor(b / 2);
+//                    if (b * 2 != c)
+//                    {
+//                        sum += candidates[r];
+//                        res += candidates[r];
+//                    }
+//                    r++;
+//                }
+//                if (sum == target)
+//                {
+//                    result.Add(res);
 //                }
 //            }
-//            numbers.Sort();
-//            for (int i = 0; i < numbers.Count; i++)
+//            HashSet<string> resultSet = new HashSet<string>();
+//            for (int i = 0; i < result.Count; i++)
 //            {
-//                if (target == 0)
-//                {
-//                    res.Add(new List<int>());
-//                    break;
-//                }
-//                sum = 0;
-//                check.Clear();
-//                sum = sum + numbers[i];
-//                check.Add(numbers[i]);
-//                for (int j = 0; j < numbers.Count; j++)
-//                {
-//                    if (sum == target)
-//                    {
-//                        res.Add(check);
-//                        break;
-//                    }
-//                    if (i == j)
-//                    {
-//                        continue;
-//                    }
-
-//                    if (sum + numbers[j] > target)
-//                    {
-//                        continue;
-//                    }
-//                    check.Add(numbers[j]);
-//                    sum += numbers[j];
-
-//                }
+//                resultSet.Add(sort(result[i]));
 //            }
-//            for (int i = 0; i < check.Count; i++)
-//            {
-//                for (int j = 0; j < check.Count - i - 1; j++)
-//                {
-//                    if (check[j] > check[j + 1])
-//                    {
-//                        int temp = check[j];
-//                        check[j] = check[j + 1];
-//                        check[j + 1] = temp;
-//                    }
-//                }
-//            }
-
-//            bool examination = true;
-//            for (int i = 0; i < res.Count; i++)
-//            {
-//                for (int j = i + 1; j < res.Count; j++)
-//                {
-//                    if (res[i] == res[j])
-//                    {
-//                        for (int k = j; k < res.Count - 1; k++)
-//                        {
-//                            res[k] = res[k + 1];
-//                        }
-//                        res--;
-//                    }
-//                    else
-//                    {
-//                        examination = false;
-//                    }
-//                    if (examination == true)
-//                    {
-//                        res.RemoveAt(i);
-//                    }
-//                    else
-//                    {
-//                        check.Add(check[i]);
-//                    }
-//                }
-//            }
-
-//            for (int i = 0; i < res.Count; i++)
-//            {
-//                Console.WriteLine(String.Join(", ", res[i]));
-
-//            }
-//            Console.WriteLine(String.Join(", ", String.Join(", ", res)));
+//            Console.WriteLine(String.Join(", ", resultSet));
 //        }
-//        check.Clear();
-
 //    }
-
 //}
 
 
